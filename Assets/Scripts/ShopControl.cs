55b353e3cb59d27140e76aa0a448424a2055eb24
erik-liang <68139423+erik-liang@users.noreply.github.com>
2025-05-28 02:29:23 -0700

    basic working implementation of shop and level selection

--- Assets/Scripts/ShopControl.cs
+++ Assets/Scripts/ShopControl.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
 
 public class ShopControl : MonoBehaviour
 {
+    public TMP_Text feedbackText;
+    public TMP_Text coinDisplay;
+
+    private int cityCost = 3;
+    private int mountainsCost = 50;
+
+    // Removed PlayerPrefs.DeleteAll() so coins won't reset every start
+
+    void Start()
+    {
+        coinDisplay.text = "Coins: " + MasterInfo.coinCount;
+    }
+
     public void SelectGameLevel()
     {
         LevelSelector.selectedLevel = "Game2";
@@ -11,13 +26,40 @@ public class ShopControl : MonoBehaviour
 
     public void SelectCityLevel()
     {
-        LevelSelector.selectedLevel = "City";
-        SceneManager.LoadScene("MainMenu");
+        HandleLevelSelection("City", cityCost);
     }
 
     public void SelectMountainsLevel()
     {
-        LevelSelector.selectedLevel = "Mountains";
-        SceneManager.LoadScene("MainMenu");
+        HandleLevelSelection("Mountains", mountainsCost);
+    }
+
+    private void HandleLevelSelection(string levelName, int cost)
+    {
+        if (LevelSelector.IsLevelUnlocked(levelName))
+        {
+            LevelSelector.selectedLevel = levelName;
+            SceneManager.LoadScene("MainMenu");
+        }
+        else if (MasterInfo.coinCount >= cost)
+        {
+            MasterInfo.coinCount -= cost;
+            LevelSelector.UnlockLevel(levelName);
+            feedbackText.text = $"{levelName} Level purchased!";
+            coinDisplay.text = "Coins: " + MasterInfo.coinCount;
+            LevelSelector.selectedLevel = levelName;
+
+            StartCoroutine(DelayedSceneLoad("MainMenu", 2f));
+        }
+        else
+        {
+            feedbackText.text = $"Not enough coins to unlock {levelName}! Need {cost}.";
+        }
+    }
+
+    private IEnumerator DelayedSceneLoad(string sceneName, float delaySeconds)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        SceneManager.LoadScene(sceneName);
     }
 }
