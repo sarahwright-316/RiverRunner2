fbe995780b397fa2d1221792d77f7ffb78e66196
Gunnar Sverdrup <gsverdrup21@gmail.com>
2025-06-02 13:47:26 -0700

    Updated to show the selected level in the background of the main menu and shop

---
+++ Assets/Scripts/ShopLevelCamera.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ShopLevelCamera : MonoBehaviour
+{
+    public Transform cameraPos;
+
+    void Start()
+    {
+        cameraPos.position = new Vector3(0, 3.2f, -20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LevelSelector.selectedLevel == "Game2")
+        {
+            cameraPos.position = new Vector3(0, 3.2f, -20);
+        }
+        else if (LevelSelector.selectedLevel == "City")
+        {
+            cameraPos.position = new Vector3(150.2f, 6, -25.3f);
+        }
+        else if (LevelSelector.selectedLevel == "Mountains")
+        {
+            cameraPos.position = new Vector3(338, 2, -29.9f);
+        }
+    }
+}
